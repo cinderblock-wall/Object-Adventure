3b7dd91c887fc5298706e5380a8d893306b7af98
cinderblock-wall <142337901+cinderblock-wall@users.noreply.github.com>
2023-12-08 13:42:30 -0500

    Character Class

--- index.js
+++ index.js
@@ -25,3 +25,28 @@ const adventurer = {
     });
 
     adventurer.roll()
+
+    // Part two: class fantasy
+
+    class Character {
+        constructor (name) {
+          this.name = name;
+          this.health = 100;
+          this.inventory = [];
+        }
+        roll (mod = 0) {
+            const result = Math.floor(Math.random() * 20) + 1 + mod;
+            console.log(`${this.name} rolled a ${result}.`)
+            }
+      }
+
+      const robin = new Character("Robin");
+      robin.inventory = ["sword", "potion", "artifact"];
+      robin.companion = new Character("Leo");
+      robin.companion.type = "Cat";
+      robin.companion.companion = new Character("Frank");
+      robin.companion.companion.type = "Flea";
+      robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+      robin.companion.roll();
+      robin.companion.companion.roll();
