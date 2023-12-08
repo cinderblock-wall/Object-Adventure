604c7ce6e836101063a4e5f2506ada6b60e895c9
cinderblock-wall <142337901+cinderblock-wall@users.noreply.github.com>
2023-12-08 13:32:56 -0500

    adventuerer1 roll

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const adventurer = {
     inventory: ["sword", "potion", "artifact"],
     companion: {
         name: "Leo",
-        type: "Cat"
-    }
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            inventory: ["small hat", "sunglasses"] 
+        }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
+        
     }
 
     // console.log(adventurer.inventory[0])
@@ -13,3 +23,5 @@ const adventurer = {
     adventurer.inventory.forEach(function(item) {
         console.log(item);
     });
+
+    adventurer.roll()
